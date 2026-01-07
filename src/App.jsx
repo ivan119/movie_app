f9c9cfe2e92996bd251549937b6419da5560c049
Ivan Kelava <ivankelava991@gmail.com>
2026-01-07 16:15:15 +0100

    chore: implement Search with debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from 'react-use';
+import Search from './components/Search.jsx';
+import MovieCard from './components/MovieCard.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { VITE_API_BASE_URL, VITE_TMDB_API_KEY } = import.meta.env;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${VITE_TMDB_API_KEY}`,
+  },
+};
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [moviesList, setMoviesList] = useState([]);
+  const [isLoadingState, setIsLoadingState] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMoves = async (query = '') => {
+    setIsLoadingState(true);
+    setErrorMessage('');
+    try {
+      const endpoint =
+        query && query.length > 2
+          ? `${VITE_API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&sort_by=popularity.desc/`
+          : `${VITE_API_BASE_URL}/discover/movie?sort_by=popularity.desc/`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('An error occurred while fetching movies.');
+      }
+      const data = await response.json();
+      if (data.Response === 'False') {
+        setErrorMessage('No movies found. Please try again with a different search term.');
+      }
+      setMoviesList(data.results || []);
+    } catch (e) {
+      console.log(e);
+      setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoadingState(false);
+    }
+  };
+  useEffect(() => {
+    fetchMoves(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="pattern"></div>
+      <div className="text-2xl">App</div>
+      <div className="wrapper">
+        <header>
+          <img src="/hero-img.png" alt="Hero banner" fetchPriority="high" loading="eager" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2 className="mt-6">All Movies</h2>
+          {isLoadingState ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-700">{errorMessage}</p>
+          ) : (
+            <ul>
+              {moviesList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
